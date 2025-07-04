5a1b388fe0006170d02ff8bbfd1321f67b096501
talk223 <t.alkattan04@gmail.com>
2025-07-04 13:04:15 -0400

    New api links for sign up and login
    This also includes the new app.tsx file which does stuffs and stuff and other stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff stuff

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,57 +1,3 @@
-// import { useState } from "react";
-
-// function Login() {
-//   const [message, setMessage] = useState("");
-//   const [loginName, setLoginName] = useState("");
-//   const [loginPassword, setPassword] = useState("");
-
-//   function handleSetLoginName(e: any): void {
-//     setLoginName(e.target.value);
-//   }
-//   function handleSetPassword(e: any): void {
-//     setPassword(e.target.value);
-//   }
-
-//   async function doLogin(event: any): Promise<void> {
-//     event.preventDefault();
-//     var obj = { login: loginName, password: loginPassword };
-//     var js = JSON.stringify(obj);
-//     try {
-//       const response = await fetch("http://karatemanager.xyz:5000/api/login", {
-//         method: "POST",
-//         body: js,
-//         headers: { "Content-Type": "application/json" },
-//       });
-//       var res = JSON.parse(await response.text());
-//       if (res.id <= 0) {
-//         setMessage("User/Password combination incorrect");
-//       } else {
-//         var user = {
-//           firstName: res.firstName,
-//           lastName: res.lastName,
-//           id: res.id,
-//         };
-//         localStorage.setItem("user_data", JSON.stringify(user));
-//         setMessage("");
-//         window.location.href = "/cards";
-//       }
-//     } catch (error: any) {
-//       alert(error.toString());
-//       return;
-//     }
-//   }
-
-//   return (
-//     <div>
-//       <span id="loginResult">{message}</span>
-//       <input type="text" placeholder="Username" className="form-control" onChange={handleSetLoginName}/>
-//       <input type="password" placeholder="Password" className="form-control" onChange={handleSetPassword} />
-//       <button className="btn btn-primary" onClick={doLogin}>Login</button>
-//     </div>
-//   );
-// }
-// export default Login;
-
 import { useState } from "react";
 import Header from "../components/Header";
 import { useNavigate } from "react-router-dom";
@@ -120,6 +66,7 @@ function Login() {
         <Header action="SIGN UP" onAction={handleSignUp} />
       </div>
       <div>
+        <span id ="loginResult">{message}</span>
         <div className="page-container">
           <div className="custom-card">
             <h1 className="card-title">ENTER KARATE TRAINER</h1>
@@ -159,4 +106,3 @@ function Login() {
   );
 }
 export default Login;
-
