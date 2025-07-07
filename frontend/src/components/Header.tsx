ac6f917bddcc1e4c0931d194e017f27911d1b575
MegavicX <houzetab@gmail.com>
2025-07-06 21:32:18 -0400

    Update Header.tsx

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,5 @@
 // import React from 'react';
+// test
 
 function Header(props: { onAction: () => void; action: string | number | bigint | boolean | React.ReactElement<unknown, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | Promise<string | number | bigint | boolean | React.ReactPortal | React.ReactElement<unknown, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | null | undefined> | null | undefined; }) {
   return (
