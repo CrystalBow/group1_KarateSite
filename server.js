23d18f19ad88884ea997d22f7d1a4b541172cc8e
Yimer Duggan <yimer.duggan@gmail.com>
2025-07-10 15:54:17 -0400

    Update server.js

--- server.js
+++ server.js
@@ -259,5 +259,25 @@ app.get('/api/verifyEmail', async (req, res) => {
     res.status(500).json('');
   }
 });
+
+app.post('/api/updateProgress', async (req, res) => {
+  const { id, progressW, progressY, progressO } = req.body;
+  let rank = 0;
+  if (progressW >= 5) {
+    rank = 1;
+    if (progressY >= 3) {
+      rank = 2;
+    }
+  }
+  let error = '';
+  try {
+    await db.collection('Users').updateOne({id: id}, {$set: {rank:rank, progressW: progressW, progressY: progressY, progressO: progressO }});
+  } catch (err) {
+    error = err.toString();
+  }
+  var ret = { rank:rank, progressW: progressW, progressY: progressY, progressO: progressO, error: error};
+  res.status(200).json(ret);
+});
+
 app.listen(5000); // start Node + Express server on port 5001
 
