6aabd171ae4e272754d6cb334e11669979e1896c
js-239 <fljds98@gmail.com>
2025-07-16 20:58:50 -0400

    Implemented edit profile api
    Allows user to edit name, email and rank. Changing email requires doing email verification again.

--- server.js
+++ server.js
@@ -266,10 +266,6 @@ app.get('/api/verifyEmail', async (req, res) => {
 
 app.post('/api/resetPassword', async (req, res) => {
   const { token, newPassword } = req.body;
-  // Check if password has required stuff Password must be at least 8 characters, include one uppercase letter, one special character, and no forbidden symbols (<, >, `, ", ', \)
-  if (newPassword.length < 8) {
-    return res.status(400).json({ error: 'Password must be more complex' });
-  }
 
   try {
     const user = await db.collection('Users').findOne({ passwordResetToken: token });
@@ -391,6 +387,66 @@ app.post('/api/deleteUser', async (req, res) =>
   catch (error)
   {
     res.status(500).json({error: "Could not delete user"})
+  }
+});
+
+// Edit user's name, email or rank
+app.post('/api/editUserInfo', async (req, res) =>
+{
+  const {user, name, email, rank} = req.body;
+  try 
+  {
+    const userBeingEdited = await db.collection('Users').findOne({user});
+    if (!userBeingEdited)
+    {
+      res.status(200).json({message: "User was not found."});
+    }
+    const editInfo = {};
+    // Edit name
+    if (name) editInfo.name = name;
+    // Edit email
+    if (email)
+    {
+      editInfo.email = email;
+      editInfo.isVerified = false;
+
+      // User must register new email
+      const newToken = crypto.randomBytes(32).toString('hex');
+      editInfo.verificationToken = newToken;
+      const verifyEmailLink = `http://143.198.160.127:5000/api/verifyEmail?token=${newToken}`;
+      // Verification email
+       const msg =
+    {
+      to: email,
+      from:
+      {
+        email: 'karatetracker@gmail.com',
+        name: 'Karate Trainer'
+      }, 
+      subject: 'Verify your Karate Trainer email',
+      html: `Hello ${name}, click <a href="${verifyEmailLink}">here</a> to verify your email. Once verified, you will be redirected to the login page.`,
+    };
+    try 
+    {
+      await mail.send(msg);
+    }
+    catch (emailError)
+    {
+      console.error("Unable to send verification email.");
+    }
+    }
+    // Update Rank
+    if (rank >= 1) 
+      editInfo.progressW = 5;
+    if (rank >= 2) 
+      editInfo.progressY = 3;
+
+    await db.collection('Users').updateOne({user}, {$set: editInfo});
+    res.status(200).json({message: "User information updated successfully."});
+  }
+  catch (error)
+  {
+    res.status(500).json({ error: "Unable to update user information." });
   }
 });
 app.listen(5000); // start Node + Express server on port 5001
