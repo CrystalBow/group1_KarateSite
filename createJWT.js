cc8d1da08993c1512de558cab1a39fed23e619fb
MegavicX <houzetab@gmail.com>
2025-07-18 22:37:05 -0400

    another attempt at updating user_data

--- createJWT.js
+++ createJWT.js
@@ -1,18 +1,18 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-exports.createToken = function (id, name, email, rank, progressW, progressY, progressO)
+exports.createToken = function (id, user, name, email, rank, streak, progressW, progressY, progressO)
 {
-    return _createToken(id, name, email, rank, progressW, progressY, progressO);
+    return _createToken(id, user, name, email, rank, streak, progressW, progressY, progressO);
 }
 // Token creatition
-_createToken = function (id, name, email, rank, progressW, progressY, progressO )
+_createToken = function (id, user, name, email, rank, streak, progressW, progressY, progressO )
 {
     try 
     {
         const expiration = new Date();
-        const user = {id, name, email, rank, progressW, progressY, progressO};
-        const accessToken = jwt.sign( user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30m'});
+        const account = {id, user, name, email, rank, streak, progressW, progressY, progressO};
+        const accessToken = jwt.sign( account, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30m'});
 
         var ret = {accessToken:accessToken};
     }
@@ -45,12 +45,14 @@ exports.refresh = function( token )
 {
     var ud = jwt.decode(token,{complete:true});
     var id = ud.payload.id;
+    var user = ud.payload.user;
     var name = ud.payload.name;
     var email = ud.payload.email;
     var rank = ud.payload.rank;
+    var streak = ud.payload.streak;
     var progressW = ud.payload.progressW;
     var progressY = ud.payload.progressY;
     var progressO = ud.payload.progressO;
 
-    return _createToken(id, name, email, rank, progressW, progressY, progressO );
+    return _createToken(id, user, name, email, rank, streak, progressW, progressY, progressO );
 }
