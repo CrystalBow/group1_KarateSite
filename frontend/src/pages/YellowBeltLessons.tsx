2b71f328cf4cc60d0121a3a4fb0057b9d312156c
MegavicX <houzetab@gmail.com>
2025-07-20 22:32:47 -0400

    Update YellowBeltLessons.tsx

--- frontend/src/pages/YellowBeltLessons.tsx
+++ frontend/src/pages/YellowBeltLessons.tsx
@@ -134,7 +134,7 @@ const YellowBeltLessons = () => {
   const handleNext = async () => {
     if (currentLessonIndex < lessons.length - 1) {
       const newLessonIndex = currentLessonIndex + 1;
-      const newUnlockedCount = Math.max(unlockedCount, newLessonIndex + 1);
+      const newUnlockedCount = Math.max(unlockedCount, newLessonIndex);
 
       setCurrentLessonIndex(newLessonIndex);
       setUnlockedCount(newUnlockedCount);
