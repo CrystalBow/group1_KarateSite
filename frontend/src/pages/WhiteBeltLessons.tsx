02ec2e864da8ae3431fdad028b258a5be7fbcc69
MegavicX <houzetab@gmail.com>
2025-07-20 20:54:11 -0400

    Update WhiteBeltLessons.tsx

--- frontend/src/pages/WhiteBeltLessons.tsx
+++ frontend/src/pages/WhiteBeltLessons.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import Header2 from "../components/Header2.tsx";
 import { storeToken } from '../tokenStorage';
-import { jwtDecode } from "jwt-decode";
+// import { jwtDecode } from "jwt-decode";
 
 const lessons = [
   {
