a792df06233b599503a75be5ac6816b9d14dfdb0
MegavicX <houzetab@gmail.com>
2025-07-22 11:23:35 -0400

    trying to fix server

--- server.js
+++ server.js
@@ -65,9 +65,7 @@ async function connectToMongo() {
 if (require.main === module) {
   connectToMongo().then(app => {
     const PORT = process.env.PORT || 5000;
-    app.listen(PORT, '0.0.0.0', () => {
-      console.log(`Server running on port ${PORT}`);
-    });
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   });
 }
 
