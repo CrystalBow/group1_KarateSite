40b93b062f13be25f8680ebb11edf6e627ca05b0
MegavicX <houzetab@gmail.com>
2025-07-23 21:20:12 -0400

    Update Header2.tsx

--- frontend/src/components/Header2.tsx
+++ frontend/src/components/Header2.tsx
@@ -226,6 +226,7 @@ function Header2(){
       // return(data.message);
       setMessage(data.message);
       setTimeout(() => setMessage(""), 1500);
+      window.location.reload();
     } catch (err) {
       console.error("Failed to fetch progress:", err);
     }
